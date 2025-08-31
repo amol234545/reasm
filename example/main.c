b8fbd04ca0c456a9d9488a02c0471906c444fd36
Aarav Sethi <aaravs@Aaravs-Mac-Studio.local>
2025-08-30 20:01:57 -0500

    Need to work off `mem/2` as stack pointer so it does not go too far forward.

--- example/main.c
+++ example/main.c
@@ -1,11 +1,60 @@
 void printf(const char *, ...);
 
-int main() {
-    float a = 3.5;
-    float b = 2.0;
-    float result = a * a + b * b;
 
-    printf("Square of %d + square of %d = %d\n", (int)a, (int)b, (int)result);
+int main() {
+    int matrix[4][4];
+    int rowSum[4] = {0};
+    int colSum[4] = {0};
+    int totalSum = 0;
+    int xorAll = 0;
+    int productDiag = 1;
+
+    // Fill the matrix with numbers 1..16
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            matrix[i][j] = i * 4 + j + 1;
+        }
+    }
+
+    // Calculate sums, XOR, and diagonal product
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            rowSum[i] += matrix[i][j];
+            colSum[j] += matrix[i][j];
+            totalSum += matrix[i][j];
+            xorAll ^= matrix[i][j];
+
+            if(i == j) {
+                productDiag *= matrix[i][j];
+            }
+        }
+    }
+
+    // Conditional outputs
+    for(int i = 0; i < 4; i++) {
+        if(rowSum[i] % 2 == 0) {
+            printf("Row %d sum is even: %d\n", i, rowSum[i]);
+        } else {
+            printf("Row %d sum is odd: %d\n", i, rowSum[i]);
+        }
+
+        if(colSum[i] % 2 == 0) {
+            printf("Column %d sum is even: %d\n", i, colSum[i]);
+        } else {
+            printf("Column %d sum is odd: %d\n", i, colSum[i]);
+        }
+    }
+
+    printf("Total sum: %d\n", totalSum);
+    printf("XOR of all elements: %d\n", xorAll);
+    printf("Product of diagonal elements: %d\n", productDiag);
+
+    // Bitwise manipulations
+    int shiftXor = 0;
+    for(int i = 0; i < 16; i++) {
+        shiftXor |= (i << (i % 4));
+    }
+    printf("Shift XOR result: %d\n", shiftXor);
 
     return 0;
 }
--- example/main.s
+++ example/main.s
@@ -7,47 +7,294 @@
 	.type	main,@function
 main:                                   # @main
 # %bb.0:
-	addi	sp, sp, -48
-	sw	ra, 44(sp)                      # 4-byte Folded Spill
-	sw	s0, 40(sp)                      # 4-byte Folded Spill
-	addi	s0, sp, 48
+	addi	sp, sp, -160
+	sw	ra, 156(sp)                     # 4-byte Folded Spill
+	sw	s0, 152(sp)                     # 4-byte Folded Spill
+	addi	s0, sp, 160
 	li	a0, 0
-	sw	a0, -28(s0)                     # 4-byte Folded Spill
+	sw	a0, -152(s0)                    # 4-byte Folded Spill
 	sw	a0, -12(s0)
-	lui	a0, 263680
-	sw	a0, -16(s0)
-	lui	a0, 262144
-	sw	a0, -20(s0)
-	lw	a0, -16(s0)
-	sw	a0, -44(s0)                     # 4-byte Folded Spill
-	lw	a1, -20(s0)
-	mv	a0, a1
-	call	__mulsf3
-	lw	a1, -44(s0)                     # 4-byte Folded Reload
-	sw	a0, -40(s0)                     # 4-byte Folded Spill
-	mv	a0, a1
-	call	__mulsf3
-	lw	a1, -40(s0)                     # 4-byte Folded Reload
-	call	__addsf3
-	sw	a0, -24(s0)
-	lw	a0, -16(s0)
-	call	__fixsfsi
-	sw	a0, -36(s0)                     # 4-byte Folded Spill
-	lw	a0, -20(s0)
-	call	__fixsfsi
-	sw	a0, -32(s0)                     # 4-byte Folded Spill
-	lw	a0, -24(s0)
-	call	__fixsfsi
-	lw	a1, -36(s0)                     # 4-byte Folded Reload
-	lw	a2, -32(s0)                     # 4-byte Folded Reload
-	mv	a3, a0
+	sw	a0, -80(s0)
+	sw	a0, -84(s0)
+	sw	a0, -88(s0)
+	sw	a0, -92(s0)
+	sw	a0, -96(s0)
+	sw	a0, -100(s0)
+	sw	a0, -104(s0)
+	sw	a0, -108(s0)
+	sw	a0, -112(s0)
+	sw	a0, -116(s0)
+	li	a1, 1
+	sw	a1, -120(s0)
+	sw	a0, -124(s0)
+	j	.LBB0_1
+.LBB0_1:                                # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_3 Depth 2
+	lw	a1, -124(s0)
+	li	a0, 3
+	blt	a0, a1, .LBB0_8
+	j	.LBB0_2
+.LBB0_2:                                #   in Loop: Header=BB0_1 Depth=1
+	li	a0, 0
+	sw	a0, -128(s0)
+	j	.LBB0_3
+.LBB0_3:                                #   Parent Loop BB0_1 Depth=1
+                                        # =>  This Inner Loop Header: Depth=2
+	lw	a1, -128(s0)
+	li	a0, 3
+	blt	a0, a1, .LBB0_6
+	j	.LBB0_4
+.LBB0_4:                                #   in Loop: Header=BB0_3 Depth=2
+	lw	a1, -124(s0)
+	slli	a0, a1, 2
+	lw	a2, -128(s0)
+	add	a0, a0, a2
+	addi	a0, a0, 1
+	slli	a3, a1, 4
+	addi	a1, s0, -76
+	add	a1, a1, a3
+	slli	a2, a2, 2
+	add	a1, a1, a2
+	sw	a0, 0(a1)
+	j	.LBB0_5
+.LBB0_5:                                #   in Loop: Header=BB0_3 Depth=2
+	lw	a0, -128(s0)
+	addi	a0, a0, 1
+	sw	a0, -128(s0)
+	j	.LBB0_3
+.LBB0_6:                                #   in Loop: Header=BB0_1 Depth=1
+	j	.LBB0_7
+.LBB0_7:                                #   in Loop: Header=BB0_1 Depth=1
+	lw	a0, -124(s0)
+	addi	a0, a0, 1
+	sw	a0, -124(s0)
+	j	.LBB0_1
+.LBB0_8:
+	li	a0, 0
+	sw	a0, -132(s0)
+	j	.LBB0_9
+.LBB0_9:                                # =>This Loop Header: Depth=1
+                                        #     Child Loop BB0_11 Depth 2
+	lw	a1, -132(s0)
+	li	a0, 3
+	blt	a0, a1, .LBB0_18
+	j	.LBB0_10
+.LBB0_10:                               #   in Loop: Header=BB0_9 Depth=1
+	li	a0, 0
+	sw	a0, -136(s0)
+	j	.LBB0_11
+.LBB0_11:                               #   Parent Loop BB0_9 Depth=1
+                                        # =>  This Inner Loop Header: Depth=2
+	lw	a1, -136(s0)
+	li	a0, 3
+	blt	a0, a1, .LBB0_16
+	j	.LBB0_12
+.LBB0_12:                               #   in Loop: Header=BB0_11 Depth=2
+	lw	a1, -132(s0)
+	slli	a2, a1, 4
+	addi	a0, s0, -76
+	add	a2, a0, a2
+	lw	a3, -136(s0)
+	slli	a3, a3, 2
+	add	a2, a2, a3
+	lw	a3, 0(a2)
+	slli	a2, a1, 2
+	addi	a1, s0, -92
+	add	a2, a1, a2
+	lw	a1, 0(a2)
+	add	a1, a1, a3
+	sw	a1, 0(a2)
+	lw	a1, -132(s0)
+	slli	a1, a1, 4
+	add	a1, a0, a1
+	lw	a2, -136(s0)
+	slli	a2, a2, 2
+	add	a1, a1, a2
+	lw	a3, 0(a1)
+	addi	a1, s0, -108
+	add	a2, a1, a2
+	lw	a1, 0(a2)
+	add	a1, a1, a3
+	sw	a1, 0(a2)
+	lw	a1, -132(s0)
+	slli	a1, a1, 4
+	add	a1, a0, a1
+	lw	a2, -136(s0)
+	slli	a2, a2, 2
+	add	a1, a1, a2
+	lw	a2, 0(a1)
+	lw	a1, -112(s0)
+	add	a1, a1, a2
+	sw	a1, -112(s0)
+	lw	a1, -132(s0)
+	slli	a1, a1, 4
+	add	a0, a0, a1
+	lw	a1, -136(s0)
+	slli	a1, a1, 2
+	add	a0, a0, a1
+	lw	a1, 0(a0)
+	lw	a0, -116(s0)
+	xor	a0, a0, a1
+	sw	a0, -116(s0)
+	lw	a0, -132(s0)
+	lw	a1, -136(s0)
+	bne	a0, a1, .LBB0_14
+	j	.LBB0_13
+.LBB0_13:                               #   in Loop: Header=BB0_11 Depth=2
+	lw	a0, -132(s0)
+	slli	a1, a0, 4
+	addi	a0, s0, -76
+	add	a0, a0, a1
+	lw	a1, -136(s0)
+	slli	a1, a1, 2
+	add	a0, a0, a1
+	lw	a1, 0(a0)
+	lw	a0, -120(s0)
+	mul	a0, a0, a1
+	sw	a0, -120(s0)
+	j	.LBB0_14
+.LBB0_14:                               #   in Loop: Header=BB0_11 Depth=2
+	j	.LBB0_15
+.LBB0_15:                               #   in Loop: Header=BB0_11 Depth=2
+	lw	a0, -136(s0)
+	addi	a0, a0, 1
+	sw	a0, -136(s0)
+	j	.LBB0_11
+.LBB0_16:                               #   in Loop: Header=BB0_9 Depth=1
+	j	.LBB0_17
+.LBB0_17:                               #   in Loop: Header=BB0_9 Depth=1
+	lw	a0, -132(s0)
+	addi	a0, a0, 1
+	sw	a0, -132(s0)
+	j	.LBB0_9
+.LBB0_18:
+	li	a0, 0
+	sw	a0, -140(s0)
+	j	.LBB0_19
+.LBB0_19:                               # =>This Inner Loop Header: Depth=1
+	lw	a1, -140(s0)
+	li	a0, 3
+	blt	a0, a1, .LBB0_28
+	j	.LBB0_20
+.LBB0_20:                               #   in Loop: Header=BB0_19 Depth=1
+	lw	a0, -140(s0)
+	slli	a1, a0, 2
+	addi	a0, s0, -92
+	add	a0, a0, a1
+	lw	a0, 0(a0)
+	srli	a1, a0, 31
+	add	a1, a0, a1
+	andi	a1, a1, -2
+	sub	a0, a0, a1
+	bnez	a0, .LBB0_22
+	j	.LBB0_21
+.LBB0_21:                               #   in Loop: Header=BB0_19 Depth=1
+	lw	a1, -140(s0)
+	slli	a2, a1, 2
+	addi	a0, s0, -92
+	add	a0, a0, a2
+	lw	a2, 0(a0)
 	lui	a0, %hi(.L.str)
 	addi	a0, a0, %lo(.L.str)
 	call	printf
-	lw	a0, -28(s0)                     # 4-byte Folded Reload
-	lw	ra, 44(sp)                      # 4-byte Folded Reload
-	lw	s0, 40(sp)                      # 4-byte Folded Reload
-	addi	sp, sp, 48
+	j	.LBB0_23
+.LBB0_22:                               #   in Loop: Header=BB0_19 Depth=1
+	lw	a1, -140(s0)
+	slli	a2, a1, 2
+	addi	a0, s0, -92
+	add	a0, a0, a2
+	lw	a2, 0(a0)
+	lui	a0, %hi(.L.str.1)
+	addi	a0, a0, %lo(.L.str.1)
+	call	printf
+	j	.LBB0_23
+.LBB0_23:                               #   in Loop: Header=BB0_19 Depth=1
+	lw	a0, -140(s0)
+	slli	a1, a0, 2
+	addi	a0, s0, -108
+	add	a0, a0, a1
+	lw	a0, 0(a0)
+	srli	a1, a0, 31
+	add	a1, a0, a1
+	andi	a1, a1, -2
+	sub	a0, a0, a1
+	bnez	a0, .LBB0_25
+	j	.LBB0_24
+.LBB0_24:                               #   in Loop: Header=BB0_19 Depth=1
+	lw	a1, -140(s0)
+	slli	a2, a1, 2
+	addi	a0, s0, -108
+	add	a0, a0, a2
+	lw	a2, 0(a0)
+	lui	a0, %hi(.L.str.2)
+	addi	a0, a0, %lo(.L.str.2)
+	call	printf
+	j	.LBB0_26
+.LBB0_25:                               #   in Loop: Header=BB0_19 Depth=1
+	lw	a1, -140(s0)
+	slli	a2, a1, 2
+	addi	a0, s0, -108
+	add	a0, a0, a2
+	lw	a2, 0(a0)
+	lui	a0, %hi(.L.str.3)
+	addi	a0, a0, %lo(.L.str.3)
+	call	printf
+	j	.LBB0_26
+.LBB0_26:                               #   in Loop: Header=BB0_19 Depth=1
+	j	.LBB0_27
+.LBB0_27:                               #   in Loop: Header=BB0_19 Depth=1
+	lw	a0, -140(s0)
+	addi	a0, a0, 1
+	sw	a0, -140(s0)
+	j	.LBB0_19
+.LBB0_28:
+	lw	a1, -112(s0)
+	lui	a0, %hi(.L.str.4)
+	addi	a0, a0, %lo(.L.str.4)
+	call	printf
+	lw	a1, -116(s0)
+	lui	a0, %hi(.L.str.5)
+	addi	a0, a0, %lo(.L.str.5)
+	call	printf
+	lw	a1, -120(s0)
+	lui	a0, %hi(.L.str.6)
+	addi	a0, a0, %lo(.L.str.6)
+	call	printf
+	li	a0, 0
+	sw	a0, -144(s0)
+	sw	a0, -148(s0)
+	j	.LBB0_29
+.LBB0_29:                               # =>This Inner Loop Header: Depth=1
+	lw	a1, -148(s0)
+	li	a0, 15
+	blt	a0, a1, .LBB0_32
+	j	.LBB0_30
+.LBB0_30:                               #   in Loop: Header=BB0_29 Depth=1
+	lw	a0, -148(s0)
+	srai	a1, a0, 31
+	srli	a1, a1, 30
+	add	a1, a0, a1
+	andi	a1, a1, -4
+	sub	a1, a0, a1
+	sll	a1, a0, a1
+	lw	a0, -144(s0)
+	or	a0, a0, a1
+	sw	a0, -144(s0)
+	j	.LBB0_31
+.LBB0_31:                               #   in Loop: Header=BB0_29 Depth=1
+	lw	a0, -148(s0)
+	addi	a0, a0, 1
+	sw	a0, -148(s0)
+	j	.LBB0_29
+.LBB0_32:
+	lw	a1, -144(s0)
+	lui	a0, %hi(.L.str.7)
+	addi	a0, a0, %lo(.L.str.7)
+	call	printf
+	li	a0, 0
+	lw	ra, 156(sp)                     # 4-byte Folded Reload
+	lw	s0, 152(sp)                     # 4-byte Folded Reload
+	addi	sp, sp, 160
 	ret
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
@@ -55,8 +302,43 @@ main:                                   # @main
 	.type	.L.str,@object                  # @.str
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .L.str:
-	.asciz	"Square of %d + square of %d = %d\n"
-	.size	.L.str, 34
+	.asciz	"Row %d sum is even: %d\n"
+	.size	.L.str, 24
+
+	.type	.L.str.1,@object                # @.str.1
+.L.str.1:
+	.asciz	"Row %d sum is odd: %d\n"
+	.size	.L.str.1, 23
+
+	.type	.L.str.2,@object                # @.str.2
+.L.str.2:
+	.asciz	"Column %d sum is even: %d\n"
+	.size	.L.str.2, 27
+
+	.type	.L.str.3,@object                # @.str.3
+.L.str.3:
+	.asciz	"Column %d sum is odd: %d\n"
+	.size	.L.str.3, 26
+
+	.type	.L.str.4,@object                # @.str.4
+.L.str.4:
+	.asciz	"Total sum: %d\n"
+	.size	.L.str.4, 15
+
+	.type	.L.str.5,@object                # @.str.5
+.L.str.5:
+	.asciz	"XOR of all elements: %d\n"
+	.size	.L.str.5, 25
+
+	.type	.L.str.6,@object                # @.str.6
+.L.str.6:
+	.asciz	"Product of diagonal elements: %d\n"
+	.size	.L.str.6, 34
+
+	.type	.L.str.7,@object                # @.str.7
+.L.str.7:
+	.asciz	"Shift XOR result: %d\n"
+	.size	.L.str.7, 22
 
 	.ident	"Homebrew clang version 20.1.8"
 	.section	".note.GNU-stack","",@progbits
